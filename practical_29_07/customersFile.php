6532e9da4d0c0f406a7f9101c4c5305ef5473910
Arturs Olekss <arturs.olekss@gmail.com>
2022-07-29 20:38:23 +0300

    29_07 (3)

--- practical_29_07/customersFile.php
+++ practical_29_07/customersFile.php
@@ -7,21 +7,42 @@
         <input type="file" name="filename">
         <button class="btn">Read</button>
     </form>
-    <?php
-    if (isset($_POST["filename"])) :
-        if (is_array(getimagesize($_POST["filename"]))) :
-            echo ("<img src='" . $_POST["filename"] . "' height=300 width=360>");
-        else :
+    <div class="container">
+        <?php
+        if (isset($_POST["filename"])) :
             $filename = $_POST["filename"];
             $file = fopen($filename, "r");
             if ($file == false) {
                 echo ("<h2 class='err'>File can't be opened'</h2>");
                 exit();
             }
-            $text = fread($file, filesize($filename));
+            //We start with the header
+            $csvContentLineArr = fgetcsv($file, filesize($filename), ";");
+            if (!$csvContentLineArr) //file is empty
+                exit();
+            echo "<b>";
+            echo "<div class='row'>";
+            foreach ($csvContentLineArr as $fieldname) :
+                echo "<div class='col'>";
+                echo $fieldname;
+                echo "</div>";
+            endforeach;
+
+            echo "</div>";
+            echo "</b>";
+
+            //Go through the content of the file
+            while ($csvContentLineArr = fgetcsv($file, filesize($filename), ";")) :
+                echo "<div class='row'>";
+                foreach ($csvContentLineArr as $fieldvalue) :
+                    echo "<div class='col'>";
+                    echo $fieldvalue;
+                    echo "</div>";
+                endforeach;
+                echo "</div>";
+            endwhile;
             fclose($file);
-            echo ("<pre>$text</pre>");
         endif;
-    endif;
-    ?>
+        ?>
+    </div>
 </body>
