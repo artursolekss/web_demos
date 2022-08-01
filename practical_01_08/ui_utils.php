38ac19eba7097931edb68d27028c954ddcf58c4a
Arturs Olekss <arturs.olekss@gmail.com>
2022-08-01 21:51:53 +0300

    01_08 (2)

---
+++ practical_01_08/ui_utils.php
@@ -0,0 +1,7 @@
+<?php
+// function printHelloName(string $firstName = "Arturs", string $lastName = "Olekss")
+// {
+//     echo "<h1>Hello " . $firstName . " " . $lastName . " " . func_get_args()[2] . "</h1>";
+// }
+
+
