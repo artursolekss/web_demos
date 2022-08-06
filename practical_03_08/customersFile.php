733fb154e1ecb049daa76589acf67e5ef12d1d35
Arturs Olekss <arturs.olekss@gmail.com>
2022-08-06 12:25:44 +0300

    06_08

--- practical_03_08/customersFile.php
+++ practical_03_08/customersFile.php
@@ -8,13 +8,13 @@ else
     echo $err;
 
 if (isset($_POST["insertFromJSFileName"]))
-    insertCustomersFromFile($_POST["insertFromJSFileName"], $con);
+    insertFromJSONFile($_POST["insertFromJSFileName"], $con);
 
 if (isset($_POST["insertFromXMLFileName"]))
-    insertCustomersFromFile($_POST["insertFromXMLFileName"], $con);
+    insertFromXML($_POST["insertFromXMLFileName"], $con);
 
 if (isset($_POST["insertFromCSVFileName"]))
-    insertCustomersFromFile($_POST["insertFromCSVFileName"], $con);
+    insertFromCSVFile($_POST["insertFromCSVFileName"], $con);
 
 if (isset($_POST["xmlFilePath"]))
     saveCustomersToXML($_POST["xmlFilePath"], $customers);
