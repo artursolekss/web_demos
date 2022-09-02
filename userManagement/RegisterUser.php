fb8a966d8660af2f9a3413bfdb49811ec5990329
Arturs Olekss <arturs.olekss@gmail.com>
2022-09-02 20:56:56 +0300

    02_09 (1)

---
+++ userManagement/RegisterUser.php
@@ -0,0 +1,32 @@
+<?php
+
+function connectToDB(string &$err = null)
+{
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $databasename = "js_06_02";
+
+    $con = new mysqli($hostname, $username, $password, $databasename);
+    if ($con->connect_error) {
+        $err = $con->connect_error;
+    }
+
+    return $con;
+}
+
+function createUser($uname, $password)
+{
+    $con = connectToDB();
+    $prepStament = $con->prepare("INSERT INTO users (username,`password`) VALUES
+    (?,?)");
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+    $prepStament->bind_param("ss", $uname, $passwordHash);
+    $prepStament->execute();
+}
+
+$newUser = json_decode(file_get_contents('php://input'));
+
+createUser($newUser->username, $newUser->password);
+
+echo json_encode($newUser);
